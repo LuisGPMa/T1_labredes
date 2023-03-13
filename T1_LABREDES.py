76bbbbd1fb60f62d1002ed6543be2a08f11fef46
LuisGPMa <43854988+LuisGPMa@users.noreply.github.com>
2023-03-13 18:55:38 -0300

    Update T1_LABREDES.py

--- T1_LABREDES.py
+++ T1_LABREDES.py
@@ -8,12 +8,12 @@ def get_mac_addr(addr):
     pass
 
 def ethernet_head(raw_data):
- dest, src, prototype = struct.unpack("!6s6s2s", raw_data[:14])
- #dest_mac = get_mac_addr(dest)
- #src_mac = get_mac_addr(src)
- #proto = socket.htons(prototype)
- data = raw_data[14:]
- return dest, src, prototype,data
+    #IPV4 TYPE: 0800
+    #IPV6 TYPE: 86DD
+    #ARP TYPE: 0806
+    dest, src, type = struct.unpack("!6s6s2s", raw_data[:14])
+    data = raw_data[14:]
+    return dest, src, type, data
 
 #IPV4 TYPE: 0800
 #IPV6 TYPE: 86DD
@@ -23,3 +23,4 @@ while True:
     eth = s.recvfrom(65565)
     parsed_eth = ethernet_head(eth[0])
     print ("Destination MAC:", binascii.hexlify(parsed_eth[0]), " Source MAC:", binascii.hexlify(parsed_eth[1]) ," Type:",binascii.hexlify(parsed_eth[2]))
+
