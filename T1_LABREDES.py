bab138294fcc4de71edb0bdbfb4a1785649e69aa
LuisGPMa <43854988+LuisGPMa@users.noreply.github.com>
2023-03-13 18:48:09 -0300

    Add files via upload

---
+++ T1_LABREDES.py
@@ -0,0 +1,22 @@
+import binascii
+import socket
+import struct
+import sys
+s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+def get_mac_addr(addr):
+    pass
+
+def ethernet_head(raw_data):
+ dest, src, prototype = struct.unpack("!6s6s2s", raw_data[:14])
+ #dest_mac = get_mac_addr(dest)
+ #src_mac = get_mac_addr(src)
+ #proto = socket.htons(prototype)
+ data = raw_data[14:]
+ return dest, src, prototype,data
+
+
+while True:
+    eth = s.recvfrom(65565)
+    parsed_eth = ethernet_head(eth[0])
+    print ("Destination MAC:", binascii.hexlify(parsed_eth[0]), " Source MAC:", binascii.hexlify(parsed_eth[1]) ," Type:",binascii.hexlify(parsed_eth[2]))
